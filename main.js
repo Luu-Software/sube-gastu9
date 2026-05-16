a5843dfc866df62b9f89ba8617b7e25bee42df42
Gastu9 <gaston.gravano@gmail.com>
2026-05-16 16:51:55 -0300

    lo termine
    soy buenisimo gto

--- main.js
+++ main.js
@@ -8,12 +8,13 @@ Distancia	  | Con SUBE registrada   | Con SUBE sin registrar
 12-27 km	  | $ 917.24		          | $ 1458.41
 */
 
+tarifa.innertext="(Tarifa)"
 boton.addEventListener("click", () => {
   // COMPLETAR
 
   let reg = registrada.value 
   let dis = distancia.value
-  let tpo = Tipo.value
+  let tpo = tipo.value
 
   if (tpo==="ciudad") {
   if (reg === "si"){
@@ -82,8 +83,167 @@ boton.addEventListener("click", () => {
     }
        }
       }
-
-    tarifa.innerText = valor;
+      //copia hasta aca
+    else {
+      if (tpo==="provincia") {
+  if (reg === "si"){
+    if(dis <= 3) {
+      valor="$ 968,57"
+     }
+      else{
+      if(dis <=6) {
+        valor="$ 1.089,64"
+      }
+        else{
+          if(dis <= 12){
+            valor="$ 1.210,71"
+          }
+          else{
+            if(dis <= 27){
+              valor="$ 1.452,85"
+            }
+            else {
+              valor="$ 1.708,07"
+            }
+          }
+        }
+      }
+    }
+  else{
+    if(reg==="no") {
+     if(dis <= 3) {
+       valor="$ 1.937,14"
+      }
+       else{
+       if(dis <=6) {
+         valor="$ 2.179,28"
+       }
+         else{
+           if(dis <= 12){
+             valor="$ 2.421,42"
+           }
+          else{
+             if(dis <= 27){
+               valor="$ 2.905,70"
+             }
+             else {
+              valor= "$ 3.416,14"
+             }
+           }
+         }
+       }
+      }
+      //tarifa
+      else{
+        if (reg==="Tarifa social"){
+        if(dis <= 3) {
+          valor="$ 435,85"
+         }
+          else{
+          if(dis <=6) {
+            valor="$ 490,33"
+          }
+            else{
+              if(dis <= 12){
+                valor="$ 544,81"
+              }
+             else{
+                if(dis <= 27){
+                  valor="$ 653,78"
+                }
+                else{
+                  valor="$ 768,63"
+                }
+              }
+            }
+          }
+      }
+    }
+       }
+      }
+      //nacional
+      else {
+      if (tpo==="nacional") {
+  if (reg === "si"){
+    if(dis <= 3) {
+      valor="$ 700,00"
+     }
+      else{
+      if(dis <=6) {
+        valor="$ 779,78"
+      }
+        else{
+          if(dis <= 12){
+            valor="$ 839,86"
+          }
+          else{
+            if(dis <= 27){
+              valor="$ 899,99"
+            }
+            else {
+              valor="$ 959,71"
+            }
+          }
+        }
+      }
+    }
+  else{
+    if(reg==="no") {
+     if(dis <= 3) {
+       valor="$ 1.113,00"
+      }
+       else{
+       if(dis <=6) {
+         valor="$ 1.239,85"
+       }
+         else{
+           if(dis <= 12){
+             valor="$ 1.335,38"
+           }
+          else{
+             if(dis <= 27){
+               valor="$ 1.430,98"
+             }
+             else {
+              valor= "$ 1.525,94"
+             }
+           }
+         }
+       }
+      }
+      //tarifa
+      else{
+        if (reg==="Tarifa social"){
+        if(dis <= 3) {
+          valor="$ 315,00"
+         }
+          else{
+          if(dis <=6) {
+            valor="$ 350,90"
+          }
+            else{
+              if(dis <= 12){
+                valor="$ 377,93"
+              }
+             else{
+                if(dis <= 27){
+                  valor="$ 404,99"
+                }
+                else{
+                  valor="$ 431,86"
+                }
+              }
+            }
+          }
+      }
+    }
+       }
+      }
+    }
+    }
+    
+    
+tarifa.innerText = valor;
 
 });
 
